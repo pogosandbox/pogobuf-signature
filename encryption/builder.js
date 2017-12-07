88a08c0b200cbe25f3bbf92153a09663cf0e8a2d
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2017-12-07 07:27:19 +0100

    0.85.1

--- encryption/builder.js
+++ encryption/builder.js
@@ -161,6 +161,7 @@ Builder.prototype.getUk25 = function() {
     else if (this.version.startsWith('0.77')) return Long.fromString('-6553495230586135539', false, 10);
     else if (this.version.startsWith('0.79')) return Long.fromString('-6553495230586135539', false, 10);
     else if (this.version.startsWith('0.83')) return Long.fromString('3081064678568720862', false, 10);
+    else if (this.version.startsWith('0.85')) return Long.fromString('3081064678568720862', false, 10);
 
     throw new Error('Unsupported encryption for version ' + this.version);
 };
